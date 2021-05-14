793737d4d2f9d2109e2c8a1d5fdacc773be78bc9
Luis Antonio Ostrowski Junior <ostrowskijr@gmail.com>
2021-05-13 23:18:17 -0300

    First Commit.

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const pdf = require('html-pdf')
+const path = require('path');
+const server = express();
+const PORT = process.env.PORT || 3000;
+
+server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'pages'));
+
+const passengers = [
+    {
+        "name" : "Luis Ostrowski",
+        "flightNumber" : 7589,
+        "time" : "14h00"
+    },
+    {
+        "name" : "Isadora Ostrowski",
+        "flightNumber" : 7569,
+        "time" : "18h00"
+    },
+    {
+        "name" : "JOão da Silva",
+        "flightNumber" : 4500,
+        "time" : "21h00"
+    },
+    {
+        "name" : "Pedro Silva",
+        "flightNumber" : 2652,
+        "time" : "13h30"
+    },
+    {
+        "name" : "Celio Ostrowski",
+        "flightNumber" : 2511,
+        "time" : "23h00"
+    },
+];
+
+server.get('/', (req, res) => {    
+    res.render('print', { passengers }, (err, html) => {
+        if (err){
+            return res.send('Erro na leitura do Arquivo! ' + err);
+        }
+        const options = {
+            height : "11.25in",
+            width : "8.5in",
+            header : {
+                height : "20mm"
+            },
+            footer : {
+                height : "20mm"
+            }
+        }
+        pdf.create(html, options).toFile('report.pdf', (err, data) => {
+            if (err) {
+                return res.send('Erro na leitura do Arquivo! ' + err);
+            }            
+        });        
+        // Enviar para o navegador.
+        return res.send(html);
+    });
+});
+
+server.listen(PORT, (error) => {
+    if (!error) {
+        console.log(`Servidor iniciado com sucesso na porta: ` + PORT);
+    } else {
+        console.log(`Erro ao iniciar servidor Node: ` + error);
+    }
+});
